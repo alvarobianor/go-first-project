5935725974d1098d8d17e2ddae0af4dbc10d80b4
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-18 19:56:09 -0300

    testing new things

--- main.go
+++ main.go
@@ -7,4 +7,13 @@ import (
 
 func main() {
 	fmt.Println("My favorite number is", utils.GetRandomNumberAlv())
+	fmt.Println("Print All things", PrintAnything(1, 3, 4,5, 6,64, 32,3 ,1, "as", '1'))
+}
+
+func PrintAnything(params ...any) []any {
+	for _, param := range params {
+		fmt.Println(param)
+	}
+
+	return params
 }
