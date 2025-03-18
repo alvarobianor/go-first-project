bce70941e24761418274279918d3f7736b6ecccb
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-18 20:14:29 -0300

    undestanding about highOrder functions

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"myFirstProject/highOrder"
 	u "myFirstProject/utils"
 )
 
 func main() {
 	fmt.Println("My favorite number is", u.GetRandomNumberAlv())
-	fmt.Println("Print All things", u.PrintAnything(1, 3, 4,5, 6,64, 32,3 ,1, "as", '1', 20.4))
+	fmt.Println("a test -> ", highOrder.Somar(2)(44))
+	// fmt.Println("Print All things", u.PrintAnything(1, 3, 4,5, 6,64, 32,3 ,1, "as", '1', 20.4))
 }
 
