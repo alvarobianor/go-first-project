cccd3421e2e1800ededcfd5d12eb7abd49dda9e7
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-18 19:58:57 -0300

    testing

--- main.go
+++ main.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"fmt"
-	"myFirstProject/utils"
+	u "myFirstProject/utils"
 )
 
 func main() {
-	fmt.Println("My favorite number is", utils.GetRandomNumberAlv())
-	fmt.Println("Print All things", PrintAnything(1, 3, 4,5, 6,64, 32,3 ,1, "as", '1'))
+	fmt.Println("My favorite number is", u.GetRandomNumberAlv())
+	fmt.Println("Print All things", u.PrintAnything(1, 3, 4,5, 6,64, 32,3 ,1, "as", '1'))
 }
 
-func PrintAnything(params ...any) []any {
-	for _, param := range params {
-		fmt.Println(param)
-	}
-
-	return params
-}
--- utils/test.go
+++ utils/test.go
@@ -1,6 +1,9 @@
 package utils
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // GetRandomNumber retorna um número aleatório
 func GetRandomNumberAlv() float64 {
@@ -10,4 +13,13 @@ func GetRandomNumberAlv() float64 {
 func generateNumber() float64 {
     var result float64 = (rand.Float64() + rand.Float64())*3
     return result
+}
+
+// A function so funny that can print anything
+func PrintAnything(params ...any) []any {
+	for _, param := range params {
+		fmt.Println(param)
+	}
+
+	return params
 }
