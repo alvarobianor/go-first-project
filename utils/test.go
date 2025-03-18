777433b747f0e24c8c7ea13e641e05b69f7547ee
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-18 19:37:19 -0300

    new tests

--- utils/test.go
+++ utils/test.go
@@ -4,5 +4,10 @@ import "math/rand"
 
 // GetRandomNumber retorna um número aleatório
 func GetRandomNumberAlv() float64 {
-    return rand.Float64()
+    return generateNumber()
+}
+
+func generateNumber() float64 {
+    var result float64 = (rand.Float64() + rand.Float64())*3
+    return result
 }
