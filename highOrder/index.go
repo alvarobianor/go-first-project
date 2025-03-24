64d43b46fdc5e519dac63fa4cce758cba9ba62af
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-24 10:12:14 -0300

    creating array

--- highOrder/index.go
+++ highOrder/index.go
@@ -11,3 +11,8 @@ func Somar(a int) func(int) int {
 func ConvertIntInString(a int) string {
 	return strconv.FormatInt(int64(a), 10)
 }
+
+// Array needs know the lenght in the declaration
+func ReturnArray() [7]int {
+	return [7]int{1, 2, 3, 4, 5, 6, 7}
+}
