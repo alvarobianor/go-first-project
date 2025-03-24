c0eef9dcfc4b0408c12ba2f7f0ed77901d11b6ef
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-03-24 10:52:16 -0300

    Creater an alternative foir loop

--- utils/fors.go
+++ utils/fors.go
@@ -11,3 +11,14 @@ func CreateForOld(leng uint8) {
 	fmt.Println("-----------------------")
 
 }
+
+func AlternativeFo1(lenght uint) uint {
+	var result uint
+	i := 0
+
+	for i < int(lenght) {
+		result += uint(i)
+		i++
+	}
+	return result
+}
